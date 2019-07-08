8ccc3bbd4667166a086927528ccfacc0e485a139
Boba-Syu <bobasyu@163.com>
2019-07-08 14:06:32 +0800

    Servlet的登录界面练习

---
+++ Servliet-Login/src/com/boba/servlet/LoginServlet.java
@@ -0,0 +1,36 @@
+package com.boba.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by MI on 2019/7/8.
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String method = request.getMethod(); // 获取请求方式
+        System.out.println("method: " + method);
+        StringBuffer url = request.getRequestURL(); // 获取url
+        System.out.println("url: " + url);
+        String uri = request.getRequestURI(); // 获取URI
+        System.out.println("uri: " + uri);
+        String scheme = request.getScheme(); // 获取传输协议(http)
+        System.out.println("scheme: " + scheme);
+        response.setContentType("text/html;charset=utf-8"); // 设置编码
+        String uid = request.getParameter("uid");
+        String pwd = request.getParameter("pwd");
+        response.getWriter().write("<html>");
+        response.getWriter().write("<head>");
+        response.getWriter().write("<title>登陆</title>");
+        response.getWriter().write("</head>");
+        response.getWriter().write("</body>");
+        response.getWriter().write("<h1>登陆成功!</h1>" +
+                                      "<p>用户id:"+uid+"</p>" +
+                                      "<p>用户密码:"+pwd+"</p>");
+        response.getWriter().write("</body>");
+        response.getWriter().write("</html>");
+    }
+}
---
+++ Servliet-Login/web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: MI
+  Date: 2019/7/8
+  Time: 13:24
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>$Title$</title>
+</head>
+<body>
+<form action="/login" method="post" style="align-items: center;"> <%-- method指定传输方法 --%>
+    user id:<input type="text" name="uid"><br>
+    password:<input type="password" name="pwd"><br>
+    <input type="submit" value="login in">
+</form>
+</body>
+</html>
